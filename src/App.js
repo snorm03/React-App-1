1d9d152f27f998fa348dd9a9e8b55a9c0e19b7a9
Sarah Norman <snorm045@gmail.com>
2025-02-03 22:12:18 -0500

    update1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const initialMenuItems=[
+    'Albert Eggstein',
+    'Attila the Hen',
+    'Dixie Chick',
+    'Gregory Peck',
+    'Mary Poopins'
+  ];
+
+  const initialMenuItemsObjects = initialMenuItems.map(
+      (item, i) => ({
+        id: i,
+        title: item
+      })
+    );
+    
   return (
     <div className="App">
       <header className="App-header">
         <p>Sidebar 1</p>
-        <Sidebar initialMenuItems={[
-            'Albert Eggstein',
-            'Attila the Hen',
-            'Dixie Chick',
-            'Gregory Peck',
-            'Mary Poopins'
-          ]}></Sidebar>
+        <Sidebar
+        initialMenuItems={initialMenuItemsObjects}
+        />
       </header>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,48 @@
 import { useState, useCallback } from "react"
+
 /**
  * Renders an array of strings passed in that can be filtered and added to as an
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
   // let [menuItems, setMenuItems] = useState(initialMenuItems)
+
+  let [menuItems, setMenuItems] = useState(initialMenuItems);
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
+    setMenuItems((prevMenuItems) => [
+      { id: prevMenuItems.length, title: newMenuItem }, 
+      ...prevMenuItems
+    ]);
     console.log("Added menu item")
+    setNewMenuItem("");
+  }, [newMenuItem]);
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
     //   setMenuItems([item, ...menuItems])
-  }, [])
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+  let filteredItems = menuItems.filter(item => 
+    item.title.toLowerCase().includes(filter.toLowerCase())
+  );
 
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
   return (
     <div>
+      <ul>
+        {filteredItems.map((item) => (
+          <li key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
       <input
         type="text"
         id="newMenuItemValue"
@@ -32,13 +50,7 @@ export default function Sidebar() {
         onChange={(event) => setNewMenuItem(event.target.value)}
       ></input>
       <br />
-      <button
-        onClick={() => {
-          /* TODO: 3 */
-        }}
-      >
-        Add Item
-      </button>
+      <button onClick={addMenuItem}>Add Item</button>
       <br />
       <input
         id="filter"
@@ -47,6 +59,7 @@ export default function Sidebar() {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+
     </div>
   )
 }
